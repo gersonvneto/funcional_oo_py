692580cf35426bd3446cc17ee41c9adc0544f180
Gerson Vasconcelos <neto_mvn@hotmail.com>
2018-04-23 20:27:42 -0300

    aula 02 de python

---
+++ aula_pyOO_2.py
@@ -0,0 +1,38 @@
+############
+# Funcional e OO
+# Prof. Neylson
+# Gerson Vasconcelos Neto
+# aula 2 - python
+###########
+
+## Objetos
+"""Vamos criar objetos,
+esses objetos possuem:
+1)métodos: a capacidade de realizar alguma ação
+2)atributos: eles sabem coisas
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
