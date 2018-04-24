55b429080aa007f6b4b7609e96f6ef492c4c8078
Gerson Vasconcelos <neto_mvn@hotmail.com>
2018-04-23 22:31:36 -0300

    aula 03 do python

---
+++ aula_pyOO_3.py
@@ -0,0 +1,39 @@
+############
+# Funcional e OO
+# Prof. Neylson
+# Gerson Vasconcelos Neto
+# aula 3 - python
+###########
+
+# Bibliotecas para análise de dados
+
+import numpy as np
+# outra importante é a scipy
+
+x = [3,5,4,6,7,9]
+print(x)
+
+len(x)
+
+np.mean(x)  #chamando o numpy para retornar a média
+np.var(x)  # variancia
+np.std(x)   # desvio padrão
+np.sqrt(x[0])   # raiz quadrada do primeiro elemento
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
