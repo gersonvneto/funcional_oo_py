bcf642d9af5befd8ecffc912fbfca2be374b98af
Gerson Vasconcelos <neto_mvn@hotmail.com>
2018-05-16 22:39:50 -0300

    aula 05

--- wvs/aula_pyOO_5.py
+++ wvs/aula_pyOO_5.py
@@ -26,34 +26,131 @@ wvs.loc[(wvs[1] == 76) |
         (wvs[2] == 12), 1].value_counts()
 
 # 9 é a variável ser feliz, transformando em NaN (sao variaveis que nao foram respondidas #checar dict de var)
-wvs.loc[wvs[9] == -2, 9] =np.nan
-wvs.loc[wvs[9] == -5, 9] =np.nan
-wvs.loc[wvs[9] == -1, 9] =np.nan
+wvs.loc[wvs[10] == -2, 10] =np.nan
+wvs.loc[wvs[10] == -5, 10] =np.nan
+wvs.loc[wvs[10] == -1, 10] =np.nan
 
 # quantidade de pessoas que responderam sobre felicidade
-wvs[9].value_counts()
+wvs[10].value_counts()
 
 # em porcentagem, arredondamos para 2 casas decimais também
-round(((wvs[9].value_counts()/\
-wvs[9].value_counts().sum()) * 100), 2)
+round(((wvs[10].value_counts()/\
+wvs[10].value_counts().sum()) * 100), 2)
 
 # onde as pessoas sao mais felizes, Brasil(variavel 76) ou EUA (variavel 840)
 
 #brasil
-round(((wvs.loc[wvs[1] == 76, 9].value_counts() /\
-wvs.loc[wvs[1] == 76, 9].value_counts().sum()) * 100), 2)
+round(((wvs.loc[wvs[1] == 76, 10].value_counts() /\
+wvs.loc[wvs[1] == 76, 10].value_counts().sum()) * 100), 2)
 
 #eua
-round(((wvs.loc[wvs[1] == 840, 9].value_counts() /\
-wvs.loc[wvs[1] == 840, 9].value_counts().sum()) * 100), 2)
+round(((wvs.loc[wvs[1] == 840, 10].value_counts() /\
+wvs.loc[wvs[1] == 840, 10].value_counts().sum()) * 100), 2)
 
-## usando o ggplot
+# comparando as média de felicidade entre BRA e EUA
+wvs.loc[wvs[1] == 840, 10].mean()
+wvs.loc[wvs[1] == 76, 10].mean()
 
-from ggplot import *
+happyBrazil = wvs.loc[wvs[1] == 76, 10]
+happyUS = wvs.loc[wvs[1] == 840, 10]
 
-ggplot(wvs, aes(x = 9)) + geom_bar()
+# nao precisa colocar a media no scipy, ele já calcula só
+t, p1 = stats.ttest_ind(happyBrazil,
+                       happyUS,
+                       nan_policy = 'omit')
 
-## com seaborn
+print(f'A estatística de teste foi {t}. \n O p-valor foi {p1}.')
+
+
+
+# comparando se a importância de política tem relação com a importância que as
+# pessoas dão para a família
+
+#### seperando as variáveis de interesse: família, amigos, tempo livre,
+# política, trabalho e religião
+
+# família
+wvs[4].value_counts()
+
+# transformando categorias de NR em NaN
+wvs.loc[wvs[4] == -2, 4] =np.nan
+wvs.loc[wvs[4] == -5, 4] =np.nan
+wvs.loc[wvs[4] == -1, 4] =np.nan
+
+wvs[4].value_counts()
+
+# política
+wvs[7].value_counts()
+
+wvs.loc[wvs[7] == -2, 7] =np.nan
+wvs.loc[wvs[7] == -5, 7] =np.nan
+wvs.loc[wvs[7] == -1, 7] =np.nan
+wvs.loc[wvs[7] == -3, 7] =np.nan
+
+wvs[4].value_counts()
+
+# plotando o valor da família e da política
+
+#1) usando seaborn (família)
+sns.set(color_codes = True)  # set serve para definir estilo do gráfico
+sns.countplot(x = wvs[4])
+plt.title('Valor dado à Família')
+plt.xlabel('Família')
+plt.ylabel('Frequência')
+plt.show()
+
+#2) usando o matplotlib   CORRIGIR
+#plt.bar(x = wvs[4], height = True)
+#plt.title('Valor dado à Família')
+#plt.xlabel('Família')
+#plt.ylabel('Frequência')
+#plt.show()
+
+#3) usando pandas
+wvs[4].value_counts().plot(kind = 'bar')
+plt.show()
+
+# plotando para política
+
+sns.set(color_codes = True)  # set serve para definir estilo do gráfico
+sns.countplot(x = wvs[7])
+plt.title('Valor dado à Política')
+plt.xlabel('Política')
+plt.ylabel('Frequência')
+plt.show()
+
+# testar se existe associção entre as duas. 
+# A forma como uma pessoa pensa sobre política tem relação com a forma
+# como essa pessoa pensa sobre família
+
+# criar uma tabela cruzada
+tabela = pd.crosstab(wvs[4], wvs[7])
+
+# teste qui-quadrado para ver a relação de variáveis categóricas
+
+from scipy import stats
+
+# fiz um unpack para cada um dos resultados que ele retorna
+chi2, p, dof, expec = stats.chi2_contingency(tabela)
+print(f'Chi-square: {chi2}')
+print(f'P-value: {p}')
+print(f'Degrees of Freedom: {dof}')
+
+###
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
